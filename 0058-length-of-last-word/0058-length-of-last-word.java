065eb3a28774f1c4125de71072310c073f1aea16
Atharva-Mulane <atharvamulane@gmail.com>
2024-11-12 22:42:02 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (6.97%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        s = s.trim();
+        
+        int lastSpaceIndex = s.lastIndexOf(' ');
+        
+        if (lastSpaceIndex == -1) {
+            return s.length();
+        }
+
+        return s.length() - lastSpaceIndex - 1;
+    }
+}
