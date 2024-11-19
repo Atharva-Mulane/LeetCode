cf53a08efe78e6f633ce88dddfeb9637c5043fb7
Atharva-Mulane <atharvamulane@gmail.com>
2024-11-19 19:57:21 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (68.92%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,7 +4,6 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
         
-        
         while(start <= end){
             int mid = start + (end - start)/ 2;
             if(nums[mid] == target){
